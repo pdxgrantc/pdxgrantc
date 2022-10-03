f4a42d9e40053b04baa014aded3f7d8a01fe3259
pdxgrantc <pdxgrantc@gmail.com>
2022-10-03 09:30:02 -0700

    added old site data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes as Switch, Route } from "react-router-dom";
+
+import Home from "./Pages/Home/Home";
+import AboutThisSite from "./Pages/AboutThisSite/AboutThisSite";
+import Code from "./Pages/Code/Code";
+import ToDoHomePage from "./Pages/ToDo/ToDoHomePage/ToDoHomePage"
+import NewItem from "./Pages/ToDo/NewItem/NewItem"
+import ShoppingList from "./Pages/ShoppingList/ShoppingList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/About-This-Site" element={<AboutThisSite />} />
+        <Route path="/Code" element={<Code />} />
+        <Route path="/Shopping-List" element={<ShoppingList />} />
+        <Route path="/ToDo" element={<ToDoHomePage />} />
+        <Route path="/ToDo/New-Item" element={<NewItem />} />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
