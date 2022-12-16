ecf3fd0a85c079cfed45ca4c7eae0545dd9bcfeb
pdxgrantc <pdxgrantc@gmail.com>
2022-12-16 08:30:51 -0800

    basic app partial

--- src/Pages/Apps/partials/AppList.js
+++ src/Pages/Apps/partials/AppList.js
@@ -5,8 +5,21 @@ import Application from "./Application";
 export default function AppList() {
     return (
         <>
-            <p>App List</p>
-            <Application />
+            <div class="w-[100%] m-auto">
+                <h2 class="text-6xl text-text_pink">Applications</h2>
+                <div class="h-[4vh]"></div>
+                <div class="flex flex-wrap gap-[3vw]">
+                    <Application />
+                    <Application />
+                    <Application />
+                    <Application />
+                    <Application />
+                    <Application />
+                    <Application />
+                    <Application />
+                    <Application />
+                </div>
+            </div>
         </>
     )
 }
--- src/Pages/Apps/partials/Application.js
+++ src/Pages/Apps/partials/Application.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
+{/* todo impliment local react router for the applications */ }
+
 export default function Application() {
     return (
         <>
-            <p>Application</p>
+            <div class="on_desktop:w-[30%] on_mobile:w-[100%] h-[40vh] bg-button_pressed_color">
+                <div class="p-[5%]">
+                    <div class="flex flex-nowrap justify-between h-min">
+                        {/* todo add link to application page */}
+                        <div>
+                            <h3 class="text-[2.5rem]">
+                                Title
+                            </h3>
+                        </div>
+                        <div>
+                            { /* align text to lower level */}
+                            <h4 class="text-[2.2rem]">
+                                GitHub Page
+                                {/* todo add github link */}
+                            </h4>
+                        </div>
+                    </div>
+                    <p class="text-[1.8rem]">This is a basic app designed to do some things which are noted later in this paragraph</p>
+                </div>
+            </div>
         </>
     )
 }
