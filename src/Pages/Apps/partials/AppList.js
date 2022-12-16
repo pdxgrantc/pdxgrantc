ba5ee0a9f3113a36fd4d5f1fa0ed7103e4a13ed3
pdxgrantc <pdxgrantc@gmail.com>
2022-12-16 08:37:45 -0800

    app list implimentation

--- src/Pages/Apps/partials/AppList.js
+++ src/Pages/Apps/partials/AppList.js
@@ -6,7 +6,7 @@ export default function AppList() {
     return (
         <>
             <div class="w-[100%] m-auto">
-                <h2 class="text-6xl text-text_pink">Applications</h2>
+                <h2 class="text-6xl text-text_teal">Applications</h2>
                 <div class="h-[4vh]"></div>
                 <div class="flex flex-wrap gap-[3vw]">
                     <Application />
--- src/Pages/Apps/partials/Application.js
+++ src/Pages/Apps/partials/Application.js
@@ -5,7 +5,7 @@ import React from 'react'
 export default function Application() {
     return (
         <>
-            <div class="on_desktop:w-[30%] on_mobile:w-[100%] h-[40vh] bg-button_pressed_color">
+            <div class="on_desktop:w-[30%] on_mobile:w-[100%] min-h-[30vh] bg-button_pressed_color">
                 <div class="p-[5%]">
                     <div class="flex flex-nowrap justify-between h-min">
                         {/* todo add link to application page */}
@@ -16,13 +16,13 @@ export default function Application() {
                         </div>
                         <div>
                             { /* align text to lower level */}
-                            <h4 class="text-[2.2rem]">
+                            <h4 class="text-[2.2rem] pt-[.3rem]">
                                 GitHub Page
                                 {/* todo add github link */}
                             </h4>
                         </div>
                     </div>
-                    <p class="text-[1.8rem]">This is a basic app designed to do some things which are noted later in this paragraph</p>
+                    <p class="text-[1.8rem] leading-[2rem]">This is a basic app designed to do some things which are noted later in this paragraph. This is a basic app designed to do some things which are noted later in this paragraph.</p>
                 </div>
             </div>
         </>
