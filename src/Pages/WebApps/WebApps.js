79564c2790163e43434028dc7428a01fc259f5da
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2023-03-07 15:11:20 -0800

    hardcode webapps.js

--- src/Pages/WebApps/WebApps.js
+++ src/Pages/WebApps/WebApps.js
@@ -5,7 +5,6 @@ import { Helmet } from "react-helmet";
 import Header from "../../Static/partials/Header";
 import Footer from "../../Static/partials/Footer";
 import Spacers from "../../Static/partials/Spacers";
-import { WebItems } from "./DataFiles/web-apps";
 
 export default function Code() {
     return (
@@ -23,16 +22,41 @@ export default function Code() {
                         <h2 className="text-6xl text-text_pink">Web Apps</h2>
                         <div className="h-[3vh]"></div>
                         <div className="grid grid-cols-2 gap-[2.5vw] on_mobile:flex on_mobile:flex-col on_mobile:gap-[5%]">
-                            {WebItems.map((data) => {
-                                return (
-                                    <Application
-                                        title={data.title}
-                                        description={data.description}
-                                        link={data.link}
-                                        extra={data.extra}
-                                    />
-                                );
-                            })}
+                            <>
+                                <a className="on_desktop:w-[100%] hover:bg-apps_bg_pressed on_desktop:min-w-[400px] w-fit h-fit on_mobile:w-[100%] min-h-[30vh] bg-apps_bg_color" href={"http://www.shopping.pdxgrantc.com"}>
+                                    <div className="p-[5%] flex flex-col gap-2">
+                                        <div className="flex flex-nowrap justify-between h-min">
+                                            <p className="text-[2.75rem]">
+                                                Shopping List
+                                            </p>
+                                        </div>
+                                        <div className="flex flex-col gap-2">
+                                            <p className="text-[1.8rem] on_desktop:max-w-[42vw] leading-[2rem]">
+                                                A web app that allows you to create a shopping list and share it with others. I has functionality to automaticallly split the price by each user for each item.
+                                            </p>
+                                            <p className="text-[1.8rem] on_desktop:max-w-[42vw] leading-[2rem]">
+                                                Currently Still Under Development
+                                            </p>
+                                        </div>
+                                    </div>
+                                </a>
+                            </>
+                            <>
+                                <a className="on_desktop:w-[100%] hover:bg-apps_bg_pressed on_desktop:min-w-[400px] w-fit h-fit on_mobile:w-[100%] min-h-[30vh] bg-apps_bg_color" href={"https://www.shopping.pdxgrantc.com/WebApps"}>
+                                    <div className="p-[5%] flex flex-col gap-2">
+                                        <div className="flex flex-nowrap justify-between h-min">
+                                            <p className="text-[2.75rem]">
+                                                ToDo
+                                            </p>
+                                        </div>
+                                        <div className="flex flex-col gap-2">
+                                            <p className="text-[1.8rem] on_desktop:max-w-[42vw] leading-[2rem]">
+                                                In Development
+                                            </p>
+                                        </div>
+                                    </div>
+                                </a>
+                            </>
                         </div>
                     </div>
                     <Spacers choice={"item_footer"} />
@@ -42,42 +66,3 @@ export default function Code() {
         </>
     );
 }
-
-function get_link(title) {
-    return title;
-}
-
-const Application = ({ title, description, link, extra }) => {
-    if (extra) {
-        return (
-            <>
-                <a className="on_desktop:w-[100%] hover:bg-apps_bg_pressed on_desktop:min-w-[400px] w-fit h-fit on_mobile:w-[100%] min-h-[30vh] bg-apps_bg_color" href={get_link(link)}>
-                    <div className="p-[5%] flex flex-col gap-2">
-                        <div className="flex flex-nowrap justify-between h-min">
-                            <p className="text-[2.75rem]">{title}</p>
-                        </div>
-                        <div className="flex flex-col gap-2">
-                            <p className="text-[1.8rem] on_desktop:max-w-[42vw] leading-[2rem]">{description}</p>
-                            <p className="text-[1.8rem] on_desktop:max-w-[42vw] leading-[2rem]">{extra}</p>
-                        </div>
-                    </div>
-                </a>
-            </>
-        );
-    } else {
-        return (
-            <>
-                <a className="on_desktop:w-[100%] hover:bg-apps_bg_pressed on_desktop:min-w-[400px] w-fit h-fit on_mobile:w-[100%] min-h-[30vh] bg-apps_bg_color" href={get_link(link)}>
-                    <div className="p-[5%] flex flex-col gap-2">
-                        <div className="flex flex-nowrap justify-between h-min">
-                            <p className="text-[2.75rem]">{title}</p>
-                        </div>
-                        <div className="flex flex-col gap-2">
-                            <p className="text-[1.8rem] on_desktop:max-w-[42vw] leading-[2rem]">{description}</p>
-                        </div>
-                    </div>
-                </a>
-            </>
-        );
-    }
-};
