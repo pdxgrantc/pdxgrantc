e32104dd13b7f0349119312939d4291edcfc0829
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2023-03-07 15:19:39 -0800

    spacing

--- src/Pages/WebApps/WebApps.js
+++ src/Pages/WebApps/WebApps.js
@@ -22,41 +22,37 @@ export default function Code() {
                         <h2 className="text-6xl text-text_pink">Web Apps</h2>
                         <div className="h-[3vh]"></div>
                         <div className="grid grid-cols-2 gap-[2.5vw] on_mobile:flex on_mobile:flex-col on_mobile:gap-[5%]">
-                            <>
-                                <a className="on_desktop:w-[100%] hover:bg-apps_bg_pressed on_desktop:min-w-[400px] w-fit h-fit on_mobile:w-[100%] min-h-[30vh] bg-apps_bg_color" href={"http://www.shopping.pdxgrantc.com"}>
-                                    <div className="p-[5%] flex flex-col gap-2">
-                                        <div className="flex flex-nowrap justify-between h-min">
-                                            <p className="text-[2.75rem]">
-                                                Shopping List
-                                            </p>
-                                        </div>
-                                        <div className="flex flex-col gap-2">
-                                            <p className="text-[1.8rem] on_desktop:max-w-[42vw] leading-[2rem]">
-                                                A web app that allows you to create a shopping list and share it with others. I has functionality to automaticallly split the price by each user for each item.
-                                            </p>
-                                            <p className="text-[1.8rem] on_desktop:max-w-[42vw] leading-[2rem]">
-                                                Currently Still Under Development
-                                            </p>
-                                        </div>
+                            <a className="on_desktop:w-[100%] hover:bg-apps_bg_pressed on_desktop:min-w-[400px] w-fit h-fit on_mobile:w-[100%] min-h-[30vh] bg-apps_bg_color" href={"http://www.shopping.pdxgrantc.com"}>
+                                <div className="p-[5%] flex flex-col gap-2">
+                                    <div className="flex flex-nowrap justify-between h-min">
+                                        <p className="text-[2.75rem]">
+                                            Shopping List
+                                        </p>
                                     </div>
-                                </a>
-                            </>
-                            <>
-                                <a className="on_desktop:w-[100%] hover:bg-apps_bg_pressed on_desktop:min-w-[400px] w-fit h-fit on_mobile:w-[100%] min-h-[30vh] bg-apps_bg_color" href={"https://www.pdxgrantc.com/WebApps"}>
-                                    <div className="p-[5%] flex flex-col gap-2">
-                                        <div className="flex flex-nowrap justify-between h-min">
-                                            <p className="text-[2.75rem]">
-                                                ToDo
-                                            </p>
-                                        </div>
-                                        <div className="flex flex-col gap-2">
-                                            <p className="text-[1.8rem] on_desktop:max-w-[42vw] leading-[2rem]">
-                                                In Development
-                                            </p>
-                                        </div>
+                                    <div className="flex flex-col gap-2">
+                                        <p className="text-[1.8rem] on_desktop:max-w-[42vw] leading-[2rem]">
+                                            A web app that allows you to create a shopping list and share it with others. I has functionality to automaticallly split the price by each user for each item.
+                                        </p>
+                                        <p className="text-[1.8rem] on_desktop:max-w-[42vw] leading-[2rem]">
+                                            Currently Still Under Development
+                                        </p>
                                     </div>
-                                </a>
-                            </>
+                                </div>
+                            </a>
+                            <a className="on_desktop:w-[100%] hover:bg-apps_bg_pressed on_desktop:min-w-[400px] w-fit h-fit on_mobile:w-[100%] min-h-[30vh] bg-apps_bg_color" href={"https://www.pdxgrantc.com/WebApps"}>
+                                <div className="p-[5%] flex flex-col gap-2">
+                                    <div className="flex flex-nowrap justify-between h-min">
+                                        <p className="text-[2.75rem]">
+                                            ToDo
+                                        </p>
+                                    </div>
+                                    <div className="flex flex-col gap-2">
+                                        <p className="text-[1.8rem] on_desktop:max-w-[42vw] leading-[2rem]">
+                                            In Development
+                                        </p>
+                                    </div>
+                                </div>
+                            </a>
                         </div>
                     </div>
                     <Spacers choice={"item_footer"} />
