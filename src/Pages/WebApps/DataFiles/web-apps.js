6fd3d51b0544c33c4ef0f4c715c1c9b860dddf1b
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2023-03-07 15:05:34 -0800

    web apps redesign

--- src/Pages/WebApps/DataFiles/web-apps.js
+++ src/Pages/WebApps/DataFiles/web-apps.js
@@ -2,8 +2,9 @@
 export const WebItems = [
     {
         title: "Shopping List",
-        description: "In Development", 
+        description: "A web app that allows you to create a shopping list and share it with others. I has functionality to automaticallly split the price by each user for each item.", 
         link: "http://www.shopping.pdxgrantc.com",
+        extra: "Currently Still Under Development"
     },
     {
         title: "ToDo",
--- src/Pages/WebApps/WebApps.js
+++ src/Pages/WebApps/WebApps.js
@@ -14,7 +14,7 @@ export default function Code() {
             <Helmet>
                 <title>Code</title>
             </Helmet>
-            <div className="m-auto bg-main_bg_color text-text_white">
+            <div className="m-auto bg-main_bg_color text-text_white min-h-screen">
                 <Header />
                 <div className="h-[2vh] on_mobile:h-[2vh] w-[35vw] on_desktop:bg-black ml-0"></div>
                 <div className="h-[5vh] on_mobile:h-[2vh] w-[35vw] ml-0 on_mobile:hidden"></div>
@@ -23,13 +23,14 @@ export default function Code() {
                     <div className="w-[100%] m-auto">
                         <h2 className="text-6xl text-text_pink">Web Apps</h2>
                         <div className="h-[3vh]"></div>
-                        <div className="flex flex-wrap justify-between on_mobile:flex-col on_mobile:gap-[5%]">
+                        <div className="grid grid-cols-2 gap-[2.5vw] on_mobile:flex on_mobile:flex-col on_mobile:gap-[5%]">
                             {WebItems.map((data) => {
                                 return (
                                     <Application
                                         title={data.title}
                                         description={data.description}
                                         link={data.link}
+                                        extra={data.extra}
                                     />
                                 );
                             })}
@@ -47,20 +48,37 @@ function get_link(title) {
     return title;
 }
 
-const Application = ({ title, description, link }) => {
-    return (
-        <>
-            <a className="on_desktop:w-[47%] hover:bg-apps_bg_pressed on_desktop:min-w-[400px] w-fit h-fit on_mobile:w-[100%] min-h-[30vh] bg-apps_bg_color" href={get_link(link)}>
-                <div className="p-[5%]">
-                    <div className="flex flex-nowrap justify-between h-min">
-                        <div>
-                            <p className="text-[2.5rem]">{title}</p>
-
+const Application = ({ title, description, link, extra }) => {
+    if (extra) {
+        return (
+            <>
+                <a className="on_desktop:w-[100%] hover:bg-apps_bg_pressed on_desktop:min-w-[400px] w-fit h-fit on_mobile:w-[100%] min-h-[30vh] bg-apps_bg_color" href={get_link(link)}>
+                    <div className="p-[5%] flex flex-col gap-2">
+                        <div className="flex flex-nowrap justify-between h-min">
+                            <p className="text-[2.75rem]">{title}</p>
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <p className="text-[1.8rem] on_desktop:max-w-[42vw] leading-[2rem]">{description}</p>
+                            <p className="text-[1.8rem] on_desktop:max-w-[42vw] leading-[2rem]">{extra}</p>
                         </div>
                     </div>
-                    <p className="text-[1.8rem] on_desktop:max-w-[42vw] leading-[2rem]">{description}</p>
-                </div>
-            </a>
-        </>
-    );
+                </a>
+            </>
+        );
+    } else {
+        return (
+            <>
+                <a className="on_desktop:w-[100%] hover:bg-apps_bg_pressed on_desktop:min-w-[400px] w-fit h-fit on_mobile:w-[100%] min-h-[30vh] bg-apps_bg_color" href={get_link(link)}>
+                    <div className="p-[5%] flex flex-col gap-2">
+                        <div className="flex flex-nowrap justify-between h-min">
+                            <p className="text-[2.75rem]">{title}</p>
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <p className="text-[1.8rem] on_desktop:max-w-[42vw] leading-[2rem]">{description}</p>
+                        </div>
+                    </div>
+                </a>
+            </>
+        );
+    }
 };
