c3c4b722c07109b43a626e2e0769f50a914fc790
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2023-03-07 11:24:58 -0800

    remove todo

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import { BrowserRouter as Router, Routes as Switch, Route } from "react-router-d
 import Home from "./Pages/Home/Home";
 import AboutThisSite from "./Pages/AboutThisSite/AboutThisSite";
 import Apps from "./Pages/Apps/Apps";
-import ToDoHomePage from "./Pages/Apps/LocalPages/ToDo/ToDoHomePage/ToDoHomePage";
 import Code from "./Pages/Code/Code";
-import NewItem from "./Pages/Apps/LocalPages/ToDo/NewItem/NewItem";
 
 
 function App() {
@@ -16,9 +14,7 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route path="/About-This-Site" element={<AboutThisSite />} />
         <Route path="/Apps" element={<Apps />} />
-        <Route path="/Apps/ToDo" element={<ToDoHomePage />} />
         <Route path="/Code" element={<Code />} />
-        <Route path="/ToDo/New-Item" element={<NewItem />} />
       </Switch>
     </Router>
   )
