2ad5f067e23fb268c19a31ba4f3bcde3ac8fa7ca
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2023-03-08 13:17:28 -0800

    header spacing

--- src/Static/partials/DeviceHeaders/DesktopHeader.js
+++ src/Static/partials/DeviceHeaders/DesktopHeader.js
@@ -5,10 +5,8 @@ export default function DesktopHeader() {
     return (
         <>
             <div className='bg-dark_grey flex justify-between'>
-
-
                 <Link className="w-[35vw] bg-black pl-[5vw] h-[8.5vh] text-[3.75rem] font-bold hover:cursor-pointer mobile:text-4xl on_desktop:whitespace-nowrap" to="/">Grant Conklin</Link>
-                <div className="pr-[5vw] flex text-[2.75rem] font-semibold mobile:text-4xl">
+                <div className="flex text-[2.75rem] font-semibold mobile:text-4xl">
                     <Link className="h-full align-middle px-[1vw] py-[.5vw] hover:bg-button_pressed_color hover:cursor-pointer hover:ease-in-out duration-[350ms]" to="/Code">Code</Link>
                     <Link className="h-full align-middle px-[1vw] py-[.5vw] hover:bg-button_pressed_color hover:cursor-pointer hover:ease-in-out duration-[350ms]" to="/WebApps">Apps</Link>
                     <Link className="h-full align-middle px-[1vw] py-[.5vw] bg-button_accent_color hover:bg-transparent hover:cursor-pointer hover:ease-in-out duration-[350ms]" to="/about-this-site">About This Site</Link>
