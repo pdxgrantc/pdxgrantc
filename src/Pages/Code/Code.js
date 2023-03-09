29d7aeb2313272c0f71d1ee94079adfed43c4eab
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2023-03-09 09:12:09 -0800

    Intro code apps

--- src/Pages/Code/Code.js
+++ src/Pages/Code/Code.js
@@ -23,12 +23,13 @@ export default function Code() {
                         <div className="w-[100%] bg-black on_desktop:gap-[2vw]">
                             <div className="flex flex-col justify-center gap-5 on_mobile:py-[4vh] on_mobile:w-[90%] on_mobile:m-auto">
                                 <h2 className="text-6xl text-text_pink">Intro</h2>
-                                <h3 className="align-middle leading-[1.1] text-[2.5rem] text-wrap">As an extension of this website I wanted to showcase my abilities to create good looking and useful web apps. This page lists all of the web applications I have written and was able to integrate into this firebase website. For more information about this site itself please refrence the link below.</h3>
+                                <h3 className="align-middle leading-[1.1] text-[2.5rem] text-wrap">These are my major projects that I would like to highlight. They are built as terminal applications which highlight my abilities with different programming languages and design sensibilities.</h3>
                                 <Link className="text-[2rem] border-b-[1.5px] on_desktop:hover:bg-button_accent_color on_desktop:hover:ease-[cubic-bezier(0.4, 0, 1, 1)] on_desktop:duration-[350ms] on_desktop:hover:px-[1.25vw] py-[.5vh] whitespace-nowrap w-min" to="/about-this-site">About This Site</Link>
                             </div>
                         </div>
                         <Spacers choice={"item_footer"} />
                     </div>
+                    
                     <div className="bg-black">
                         <div>
                             <Spacers choice={"item_header"} />
--- src/Pages/WebApps/WebApps.js
+++ src/Pages/WebApps/WebApps.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
 
 // Partials
 import Header from "../../Static/partials/Header";
@@ -17,8 +18,20 @@ export default function Code() {
                 <div className="on_mobile:h-[2vh] w-[35vw] ml-0 on_mobile:hidden"></div>
                 <div className="on_desktop:bg-black on_desktop:px-[3vw]">
                     <Spacers choice={"item_header"} />
+                    <div className="w-[100%] bg-black on_desktop:gap-[2vw]">
+                        <div className="flex flex-col justify-center gap-5 on_mobile:py-[4vh] on_mobile:w-[90%] on_mobile:m-auto">
+                            <h2 className="text-6xl text-text_pink">Intro</h2>
+                            <h3 className="align-middle leading-[1.1] text-[2.5rem] text-wrap">As an extension of this website I wanted to showcase my abilities to create good looking and useful web apps. This page lists all of the web applications I have written and was able to integrate into this firebase website. For more information about this site itself please refrence the link below.</h3>
+                            <Link className="text-[2rem] border-b-[1.5px] on_desktop:hover:bg-button_accent_color on_desktop:hover:ease-[cubic-bezier(0.4, 0, 1, 1)] on_desktop:duration-[350ms] on_desktop:hover:px-[1.25vw] py-[.5vh] whitespace-nowrap w-min" to="/about-this-site">About This Site</Link>
+                        </div>
+                    </div>
+                    <Spacers choice={"item_footer"} />
+                </div>
+                <div className="h-[4vh]"></div>
+                <div className="on_desktop:bg-black on_desktop:px-[3vw]">
+                    <Spacers choice={"item_header"} />
                     <div className="w-[100%] m-auto">
-                        <h2 className="text-6xl text-text_pink">Web Apps</h2>
+                        <h2 className="text-6xl text-text_teal">Web Apps</h2>
                         <div className="h-[3vh]"></div>
                         <div className="grid grid-cols-2 gap-[2.5vw] on_mobile:flex on_mobile:flex-col on_mobile:gap-[5%]">
                             <a className="rounded-low on_desktop:w-[100%] hover:bg-apps_bg_pressed on_desktop:min-w-[400px] w-fit h-fit on_mobile:w-[100%] min-h-[30vh] bg-apps_bg_color" href={"http://www.shopping.pdxgrantc.com"}>
