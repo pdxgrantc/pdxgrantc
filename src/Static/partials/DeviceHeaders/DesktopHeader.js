3e6c8d87adfd6b02e3d719d5dc712c30f3560977
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2023-03-09 09:01:28 -0800

    device header fix

--- src/Static/partials/DeviceHeaders/DesktopHeader.js
+++ src/Static/partials/DeviceHeaders/DesktopHeader.js
@@ -5,8 +5,8 @@ export default function DesktopHeader() {
     if (window.location.pathname === "/") {
         return (
             <>
-                <div className='bg-dark_grey flex justify-between'>
-                    <Link className="leading-[5rem] w-[35vw] bg-black pl-[5vw] h-[8.5vh] text-[3.75rem] font-bold hover:cursor-pointer mobile:text-4xl on_desktop:whitespace-nowrap" to="/">Grant Conklin</Link>
+                <div className='bg-dark_grey flex justify-between h-[10vh]'>
+                    <Link className="h-full leading-[5rem] w-[35vw] bg-black pl-[5vw] text-[3.75rem] font-bold hover:cursor-pointer mobile:text-4xl on_desktop:whitespace-nowrap" to="/">Grant Conklin</Link>
                     <div className="flex text-[2.75rem] font-semibold mobile:text-4xl">
                         <Link className="h-full align-middle px-[1vw] py-[.5vw] hover:bg-button_pressed_color hover:cursor-pointer hover:ease-in-out duration-[350ms]" to="/Code">Code</Link>
                         <Link className="h-full align-middle px-[1vw] py-[.5vw] hover:bg-button_pressed_color hover:cursor-pointer hover:ease-in-out duration-[350ms]" to="/WebApps">Apps</Link>
@@ -19,8 +19,8 @@ export default function DesktopHeader() {
     else {
         return (
             <>
-                <div className='bg-dark_grey flex justify-between'>
-                    <Link className="leading-[5rem] w-[35vw] bg-black pl-[5vw] h-[8.5vh] text-[3.75rem] font-bold hover:cursor-pointer mobile:text-4xl on_desktop:whitespace-nowrap" to="/">Grant Conklin</Link>
+                <div className='bg-dark_grey flex justify-between h-[10vh]'>
+                    <Link className="h-full leading-[5rem] w-[35vw] bg-black pl-[5vw] text-[3.75rem] font-bold hover:cursor-pointer mobile:text-4xl on_desktop:whitespace-nowrap" to="/">Grant Conklin</Link>
                     <div className="flex text-[2.75rem] font-semibold mobile:text-4xl">
                         <Link className="h-full align-middle px-[1vw] py-[.5vw] hover:bg-button_pressed_color hover:cursor-pointer hover:ease-in-out duration-[350ms]" to="/Code">Code</Link>
                         <Link className="h-full align-middle px-[1vw] py-[.5vw] hover:bg-button_pressed_color hover:cursor-pointer hover:ease-in-out duration-[350ms]" to="/WebApps">Apps</Link>
